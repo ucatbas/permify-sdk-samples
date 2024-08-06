ce64b85daf86233ba395fee7dae4936154dd05e5
ufuk <ufukcivan4@gmail.com>
2024-08-06 11:40:22 +0300

    js-grpc update

--- javascript/grpc/create_tenant.js
+++ javascript/grpc/create_tenant.js
@@ -1,24 +1,16 @@
-const service_grpc = require('@buf/permifyco_permify.grpc_node/base/v1/service_grpc_pb');
-const service = require('@buf/permifyco_permify.grpc_node/base/v1/service_pb');
-const { ChannelCredentials } = require('@grpc/grpc-js');
+const permify = require("@permify/permify-node");
 
-// Create a new client instance
-const tenancyClient = new service_grpc.TenancyClient("127.0.0.1:3478", ChannelCredentials.createInsecure());
+const request = new permify.grpc.payload.TenantCreateRequest();
+request.setId("t13333");
+request.setName("lsjv");
 
-// Create a new TenantCreateRequest
-const request = new service.TenantCreateRequest();
-const timestamp = new Date().getTime().toString();
-
-// Set tenant ID and name
-let tenantId = "tenant_" + timestamp;
-request.setId(tenantId);
-request.setName("Tenant 1");
-
-// Call the create method on the client
-tenancyClient.create(request, (error, response) => {
-    if (error) {
-        console.error("Error:", error.message);
-    } else {
-        console.log("Response:", response.toObject());
-    }
+const client = permify.grpc.newClient({
+    endpoint: "localhost:3478",
+    cert: undefined
 });
+
+client.tenancy.create(request).then((response) => {
+    console.log(response);
+
+    // handle response
+})
