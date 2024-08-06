9fb3d3ddddb2684ef4a1acbc317be314778a14f7
ufuk <ufukcivan4@gmail.com>
2024-08-06 11:34:16 +0300

    ts-grpc update

--- typescript/grpc/create_tenant.ts
+++ typescript/grpc/create_tenant.ts
@@ -1,23 +1,17 @@
-import * as service_grpc from '@buf/permifyco_permify.grpc_node/base/v1/service_grpc_pb';
-import * as service from '@buf/permifyco_permify.grpc_node/base/v1/service_pb';
-import { ChannelCredentials, ServiceError } from "@grpc/grpc-js";
+import * as permify from "@permify/permify-node";
 
-const tenancyClient = new service_grpc.TenancyClient("127.0.0.1:3478", ChannelCredentials.createInsecure());
 
-const request = new service.TenantCreateRequest();
-const timestamp = new Date().getTime().toString();
+const request = new permify.grpc.payload.TenantCreateRequest();
+request.setId("t13333");
+request.setName("lsjv");
 
-let tenantId = "tenant_" + timestamp; 
-request.setId(tenantId);
-request.setName( "Tenant 1");
-
-// Call the create method directly
-tenancyClient.create(request, (error: ServiceError | null, response: service.TenantCreateResponse | null) => {
-    if (error) {
-        console.error("Error:", error);
-    } else {
-        console.log(response.toObject());
-    }
+const client = permify.grpc.newClient({
+    endpoint: "localhost:3478",
+    cert: undefined
 });
 
+client.tenancy.create(request).then((response: any) => {
+    console.log(response);
 
+    // handle response
+})
